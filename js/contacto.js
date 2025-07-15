cfd69eeb5f4da0b13bab1d068f03c2d018d807b2
Salvador Fernando <newvibe025@gmail.com>
2025-07-16 00:32:49 +0100

    alteração nas páginas de sobre e contacto

--- js/contacto.js
+++ js/contacto.js
@@ -6,24 +6,24 @@
 
     const formData = new FormData(form);
 
-    fetch("https://formsubmit.co/ajax/gconecta05angola@gmail.com", {
+    fetch("https://formsubmit.co/ajax/a24b7a5e3f57eb6b28a83fb07425b9a6", {
       method: "POST",
       body: formData,
       headers: {
-        Accept: "application/json"
-      }
+        Accept: "application/json",
+      },
     })
-    .then(response => {
-      if (response.ok) {
-        popup.classList.remove("hidden");
-        form.reset();
-        setTimeout(() => popup.classList.add("hidden"), 5000);
-      } else {
-        alert("Erro ao enviar. Tente novamente.");
-      }
-    })
-    .catch(() => {
-      alert("Erro de conexão. Verifique sua internet.");
-    });
+      .then((response) => {
+        if (response.ok) {
+          popup.classList.remove("hidden");
+          form.reset();
+          setTimeout(() => popup.classList.add("hidden"), 5000);
+        } else {
+          alert("Erro ao enviar. Tente novamente.");
+        }
+      })
+      .catch(() => {
+        alert("Erro de conexão. Verifique sua internet.");
+      });
   });
 
